bc147aa9347391a6141eb2ad7b267fddfc7e0fb5
Peter Armstrong <peter@ruboss.com>
2016-02-19 00:44:10 -0800

    getting basic info

--- web.rb
+++ web.rb
@@ -1,12 +1,12 @@
 require 'sinatra'
 require 'json'
 
-# Snake ID ee78439a-e6fa-40a0-a28a-874b7b10e287
+MY_SNAKE_ID = "ee78439a-e6fa-40a0-a28a-874b7b10e287"
 
 get '/' do
     responseObject = {
         "color"=> "#eeeeee",
-        "head_url"=> "https://leanpub-battlesnake.herokuapp.com/leanpublogo.png"
+        "head_url"=> "https://leanpub-battlesnake.herokuapp.com/public/images/logo.png"
     }
 
     return responseObject.to_json
@@ -20,7 +20,7 @@ post '/start' do
 
     # Dummy response
     responseObject = {
-        "taunt" => "battlesnake-ruby",
+        "taunt" => "hello taunt",
     }
 
     return responseObject.to_json
@@ -31,13 +31,45 @@ post '/move' do
     requestJson = requestBody ? JSON.parse(requestBody) : {}
 
     # Calculate a move with the request data
-    puts "debug hello world"
+    puts requestJson
 
+    turn = requestJson["turn"]
+    food = requestJson["food"]
+    gold = requestJson["gold"]
+    mode = requestJson["mode"]
+    walls = requestJson["walls"]
+    height = requestJson["height"]
+    game = requestJson["game"]
+    snakes = requestJson["snakes"]
+    width = requestJson["width"]
+
+    my_snake = snakes.select{|snake| snake["id"] == MY_SNAKE_ID}.first
+    other_snakes = snakes.reject{|snake| snake["id"] == MY_SNAKE_ID}
+
+    puts "turn is #{turn}"
+    puts "food is #{food}"
+    puts "gold is #{gold}"
+    puts "mode is #{mode}"
+    puts "walls is #{walls}"
+    puts "height is #{height}"
+    puts "game is #{game}"
+    puts "snakes are #{snakes}"
+    puts "width is #{width}"
+    puts "my_snake is #{my_snake}"
+    puts "other_snakes are #{other_snakes}"
+
+# {"turn": 2, "food": [], "gold": [[10, 9]], "mode": "advanced", "walls": [], "height": 20, "game": "inspired-runoff", "snakes": [{"gold": 0, "url": "localsnake://greg", "kills": 0, "age": 2, "id": "4ac21049-218e-421b-b4ef-c095e5032647", "message": "", "name": "Sleepy Snake", "coords": [[14, 5], [14, 4], [15, 4]], "taunt": null, "status": "alive", "health": 98}, {"id": "ee78439a-e6fa-40a0-a28a-874b7b10e287", "message": "", "name": "Lean Snake", "coords": [[15, 13], [15, 14], [15, 15]], "taunt": "going north!", "health": 98, "status": "alive", "gold": 0, "kills": 0, "age": 2}], "width": 20}
+
+    my_snake_coords = my_snake["coords"]
+
+
+    moves = %w(north east south west)
+    move = moves[rand(4)]
 
     # Dummy response
     responseObject = {
-      "move" => "north", # One of either "north", "east", "south", or "west".
-      "taunt" => "going north!",
+      "move" => move,
+      "taunt" => "going #{move}!"
     }
 
     return responseObject.to_json
